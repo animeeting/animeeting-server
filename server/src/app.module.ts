c4a2cfa6567d7ca76c23d35493397a0426e2fcb3
agustinhopneto <agustinho.pneto@gmail.com>
2021-08-17 20:49:01 -0300

    add create users table migration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnilistModule } from './modules/anilist/anilist.module';
 import { UsersModule } from './modules/users/users.module';
-
 @Module({
-  imports: [AnilistModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      //@ts-ignore
+      type: process.env.TYPEORM_CONNECTION,
+      host: process.env.TYPEORM_APPHOST,
+      port: Number(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [],
+      synchronize: false,
+    }),
+    AnilistModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
