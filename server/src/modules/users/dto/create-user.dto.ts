b0d2b10c0fcfdfd46ca17bab37350681d4edab19
agustinhopneto <agustinho.pneto@gmail.com>
2021-08-18 19:54:35 -0300

    add data validation to create users

--- server/src/modules/users/dto/create-user.dto.ts
+++ server/src/modules/users/dto/create-user.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, Length } from 'class-validator';
+
 export class ICreateUserDTO {
+  @Length(2, 100)
   name: string;
+
+  @Length(2, 100)
   nickname: string;
+
+  @IsEmail()
+  @Length(0, 200)
   email: string;
+
+  @Length(8, 24)
   password: string;
 }
